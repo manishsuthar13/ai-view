44369c4a6b6a607a146a0d2e18374390b9e14d50
manishsuthar13 <manisuthar.10@gmail.com>
2026-07-28 23:35:13 +0530

    Add button styles and enhance form layouts for login and register pages

--- frontend/src/features/auth/pages/Login.jsx
+++ frontend/src/features/auth/pages/Login.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import "../auth.form.scss"
+import { Link } from 'react-router'
 
 const Login = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
   return (
     <main>
       <div className="form-container">
         <h1>Login</h1>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="input-group">
             <label htmlFor="email">Email</label>
             <input type="email" id="email" placeholder="Enter your email" />
@@ -18,6 +23,7 @@ const Login = () => {
           </div>
           <button className="button primary-button" type="submit">Login</button>
         </form>
+        <p>Don't have an account? <Link to={"/register"}>Register</Link> </p>
       </div>
     </main>
   )
