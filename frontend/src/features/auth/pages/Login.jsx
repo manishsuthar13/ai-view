7692a2ec676b0038921fde0e49fcf946a6f2f67e
manishsuthar13 <manisuthar.10@gmail.com>
2026-07-30 02:07:35 +0530

    Add authentication context and protected routes; integrate axios for API calls

--- frontend/src/features/auth/pages/Login.jsx
+++ frontend/src/features/auth/pages/Login.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import { useState } from 'react'
 import "../auth.form.scss"
 import { Link } from 'react-router'
+import { useAuth } from '../hooks/useAuth'
+import { useNavigate } from 'react-router'
 
 const Login = () => {
 
-  const handleSubmit = (e) => {
+  const { loading, handleLogin } = useAuth();
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState ('');
+  const [password, setPassword] = useState ('');
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    await handleLogin({email, password});
+    navigate('/')
   }
+
+  if (loading){
+    return(<main><h1>Loading...</h1></main>)
+  }
+
   return (
     <main>
       <div className="form-container">
@@ -15,11 +31,15 @@ const Login = () => {
         <form onSubmit={handleSubmit}>
           <div className="input-group">
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" placeholder="Enter your email" />
+            <input
+            onChange={(e) => setEmail(e.target.value)} 
+            type="email" id="email" placeholder="Enter your email" />
           </div>
           <div className="input-group">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" name="password" placeholder="Enter your password" />
+            <input 
+            onChange={(e) => setPassword(e.target.value)}
+            type="password" id="password" name="password" placeholder="Enter your password" />
           </div>
           <button className="button primary-button" type="submit">Login</button>
         </form>
--- frontend/src/features/auth/pages/Register.jsx
+++ frontend/src/features/auth/pages/Register.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate, Link } from 'react-router'
-
+import { useAuth } from '../hooks/useAuth'
 const Register = () => {
 
   const navigate = useNavigate();
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { loading, handleRegister } = useAuth();
 
-  const handleSubmit = (e) => {
+
+  const handleSubmit = async(e) => {
     e.preventDefault();
+    await handleRegister({username, email, password});
+    navigate('/')
   }
+
+  if (loading){
+    return(<main><h1>Loading...</h1></main>)
+  }
+
   return (
     <main>
       <div className="form-container">
@@ -16,15 +28,21 @@ const Register = () => {
         <form onSubmit={handleSubmit}>
         <div className="input-group">
             <label htmlFor="username">Username</label>
-            <input type="text" id="username" name="username" placeholder="Enter your Username" />
+            <input 
+            onChange={(e) => setUsername(e.target.value)}
+            type="text" id="username" name="username" placeholder="Enter your Username" />
           </div>
           <div className="input-group">
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" name="email" placeholder="Enter your email" />
+            <input 
+            onChange={(e) => setEmail(e.target.value)}
+            type="email" id="email" name="email" placeholder="Enter your email" />
           </div>
           <div className="input-group">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" name="password" placeholder="Enter your password" />
+            <input 
+            onChange={(e) => setPassword(e.target.value)}
+            type="password" id="password" name="password" placeholder="Enter your password" />
           </div>
           <button className="button primary-button" type="submit">Register</button>
         </form>
